11aa13884f3580b8636e102cf89e060eed3dc0b2
Kalpitrao47 <kalpitraorane47@gmail.com>
2024-02-19 10:55:04 +0530

    Table Created

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,64 @@ import React from 'react'
 
 const Table = () => {
   return (
-    <div>Table</div>
+    <div className='flex justify-center p-12'>
+    <table className="table-auto">
+  <thead>
+  <tr>BORIVALI AFTERNOON ROUTE</tr>
+  <tr>DECEMBER 2023</tr>
+    <tr>
+      <th>SR.NO</th>
+      <th>DATE</th>
+      <th>VEHICAL</th>
+      <th>TRIP SHEET</th>
+      <th>DEL TRAY</th>
+      <th>RET TRAY</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1.</td>
+      <td>12-12-2024</td>
+      <td>BMW</td>
+      <td>anon</td>
+      <td>ug</td>
+      <td>bdj</td>
+    </tr>
+    <tr>
+      <td>1.</td>
+      <td>12-12-2024</td>
+      <td>BMW</td>
+      <td>anon</td>
+      <td>ug</td>
+      <td>bdj</td>
+    </tr>
+    <tr>
+      <td>1.</td>
+      <td>12-12-2024</td>
+      <td>BMW</td>
+      <td>anon</td>
+      <td>ug</td>
+      <td>bdj</td>
+    </tr>
+    <tr>
+      <td>1.</td>
+      <td>12-12-2024</td>
+      <td>BMW</td>
+      <td>anon</td>
+      <td>ug</td>
+      <td>bdj</td>
+    </tr>
+    <tr>
+      <td>1.</td>
+      <td>12-12-2024</td>
+      <td>BMW</td>
+      <td>anon</td>
+      <td>ug</td>
+      <td>bdj</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
   )
 }
 
